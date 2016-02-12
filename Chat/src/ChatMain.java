6edb652713d7b20e6bae9687464a9372a8f823b7
Eric Rogers <eric.j.rogers18@gmail.com>
2016-02-12 12:30:58 -0500

    Updated Chat

--- Chat/src/ChatMain.java
+++ Chat/src/ChatMain.java
@@ -1,19 +1,28 @@
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Vector;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JTabbedPane;
-import javax.swing.JTextArea;
+import javax.swing.JTable;
+import javax.swing.ListSelectionModel;
+import javax.swing.ScrollPaneConstants;
 import javax.swing.UIManager;
 import javax.swing.border.MatteBorder;
+import javax.swing.table.TableColumn;
 
-public class ChatMain {
+public class ChatMain implements ActionListener {
 	private static ChatMain m_instance = null;
-	
-//	public static void main(String args[]) {
-//		new ChatMain();
-//	}
+	private JTabbedPane tabPane;
+	private JTable userTable;
+	private JButton startGroup;
 	
 	private ChatMain() {
 		try{
@@ -38,31 +47,54 @@ public class ChatMain {
 		leftPanel.setBorder(new MatteBorder(5, 5, 5, 0, Color.BLACK));
 		mainPanel.add(leftPanel, BorderLayout.WEST);
 		
+		//CustomTableModel model = new CustomTableModel();
+		Vector<String> names = new Vector<String>();
+		names.add("Select");
+		names.add("UserName");
+		Vector<String> data = new Vector<String>();
+		userTable = new JTable(data, names);
+		Font tableFont = new Font("", Font.BOLD, 15);
+		userTable.setFont(tableFont);
+		userTable.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		userTable.setRowSelectionAllowed(true);
+		userTable.setRowHeight(35);
+		JScrollPane tablePane = new JScrollPane(userTable, ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED,
+		ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+		tablePane.setPreferredSize(new Dimension(228, 480));
+		userTable.getColumnModel().getColumn(0).setPreferredWidth(50);
+		userTable.getColumnModel().getColumn(1).setPreferredWidth(120);
+		leftPanel.add(tablePane);
+		
+		startGroup = new JButton("Start Group");
+		startGroup.setPreferredSize(new Dimension(200, 50));
+		startGroup.addActionListener(this);
+		leftPanel.add(startGroup);
+		
 		JPanel rightPanel = new JPanel();
 		rightPanel.setPreferredSize(new Dimension(750, 600));
 		rightPanel.setBackground(new Color(0, 0, 0));
 		mainPanel.add(rightPanel);
 		
-		JTabbedPane tabPane = new JTabbedPane();
+		tabPane = new JTabbedPane();
 		tabPane.setPreferredSize(new Dimension(745, 592));
 		tabPane.setBackground(new Color(40, 40, 40));
 		rightPanel.add(tabPane, BorderLayout.EAST);
 		tabPane.addTab("All Clients", new ChatTab());
-
-
-		
-
-		
-
-		
 		
 		mainFrame.setVisible(true);
 	}
 	
-	private void createTab(String title, JTabbedPane tabPane) {
+	private void createTab(String title) {
 		tabPane.addTab(title, new ChatTab());
 	}
-	
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if(e.getSource() == startGroup) {
+			createTab("Group Chat");
+		}
+	}
+    
 	public static ChatMain startChat() {
 		if(m_instance == null) {
 			m_instance = new ChatMain();
---
+++ Chat/src/User.java
@@ -0,0 +1,23 @@
+
+public class User {
+	
+	String userName;
+	Boolean select = false;
+	
+	public User(String name) {
+		userName = name;
+	}
+	
+	public void setUserName() {
+		
+	}
+	
+	public String getUserName() {
+		return userName;
+	}
+	
+	public Boolean getBoolean() {
+		return select;
+	}
+
+}
