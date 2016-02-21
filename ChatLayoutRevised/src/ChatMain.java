62116eb103c661939ef1cef7f3f775ac86b6647e
nsteward96 <treck96@gmail.com>
2016-02-21 02:56:04 -0500

    Added a lot of networking stuff, can't run in current state, a couple errors stopping from further compilation. Need help on fixing them - gotta find a way to pass in a file reference, etc. Once it can run, we should be just about or almost ready to chat.

--- ChatLayoutRevised/src/ChatMain.java
+++ ChatLayoutRevised/src/ChatMain.java
@@ -15,35 +15,49 @@ import javax.swing.ListSelectionModel;
 import javax.swing.ScrollPaneConstants;
 import javax.swing.UIManager;
 import javax.swing.border.MatteBorder;
+import java.util.*;
+import java.net.*;
+import java.io.*;
 
-public class ChatMain implements ActionListener {
+public class ChatMain implements ActionListener, MessageListener {
 	private static ChatMain m_instance = null;
 	private JTabbedPane tabPane;
 	private JTable userTable;
 	private JButton startGroup;
-	
+
 	private ChatMain() {
-		try{
+		try {
 			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
 		}
 		catch(Exception e) {
-			e.printStackTrace(); 
+			e.printStackTrace();
 		}
-		
+		try {	//Sets up connection stuff
+			Socket socket = new Socket("localhost", 4324);
+			ObjectOutputStream out = new ObjectOutputStream(socket.getOutputStream());
+			ObjectInputStream in = new ObjectInputStream(socket.getInputStream());
+			MessageReceiver mr = new MessageReceiver(in, this);
+			Thread t = new Thread(mr);
+			t.start();
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+
 		JFrame mainFrame = new JFrame("Chat");
 		mainFrame.setSize(new Dimension(1000, 600));
 		mainFrame.setResizable(false);
-		
+
 		JPanel container = new JPanel(new BorderLayout());
 		container.setSize(new Dimension(1000, 600));
 		container.setBackground(Color.BLACK);
-		
+
 		JPanel leftPanel = new JPanel();
 		leftPanel.setPreferredSize(new Dimension(275, 545));
 		leftPanel.setBackground(new Color(40, 40, 40));
 		leftPanel.setBorder(new MatteBorder(5, 5, 5, 0, Color.BLACK));
 		container.add(leftPanel, BorderLayout.WEST);
-		
+
 		Vector<String> columnNames = new Vector<String>();
 		columnNames.add("Select");
 		columnNames.add("UserName");
@@ -60,28 +74,36 @@ public class ChatMain implements ActionListener {
 		userTable.getColumnModel().getColumn(0).setPreferredWidth(40);
 		userTable.getColumnModel().getColumn(1).setPreferredWidth(150);
 		leftPanel.add(tablePane);
-		
+
 		startGroup = new JButton("Start Group");
 		startGroup.setPreferredSize(new Dimension(265, 40));
 		startGroup.addActionListener(this);
 		leftPanel.add(startGroup);
-		
+
 		JPanel rightPanel = new JPanel();
 		rightPanel.setPreferredSize(new Dimension(700, 547));
 		rightPanel.setBackground(new Color(0, 0, 0));
 		container.add(rightPanel, BorderLayout.EAST);
-		
+
 		tabPane = new JTabbedPane();
 		tabPane.setPreferredSize(new Dimension(694, 539));
 		tabPane.setBackground(new Color(40, 40, 40));
 		rightPanel.add(tabPane);
 		tabPane.addTab("All Clients", new ChatTab());
-		
+
 		mainFrame.add(container);
 		mainFrame.pack();
 		mainFrame.setVisible(true);
 	}
-	
+
+	public void deliverMessage(String s) {
+		messageArea.append(s); //Needs to see 'messageArea' JTextArea in ChatTab.
+	}
+
+	public void removeMe() {
+		System.exit(0);
+	}
+
 	private void createTab(String title) {
 		tabPane.addTab(title, new ChatTab());
 	}
@@ -92,7 +114,7 @@ public class ChatMain implements ActionListener {
 			createTab("Group Chat");
 		}
 	}
-    
+
 	public static ChatMain startChat() {
 		if(m_instance == null) {
 			m_instance = new ChatMain();
--- ChatLayoutRevised/src/ChatTab.java
+++ ChatLayoutRevised/src/ChatTab.java
@@ -13,36 +13,38 @@ import javax.swing.JTextArea;
 import javax.swing.ScrollPaneConstants;
 
 public class ChatTab extends JPanel implements ActionListener {
-	
+
 	JButton sendButton;
-	
+	String request = "";
+
 	public ChatTab() {
 
 		Font textFont = new Font("", Font.PLAIN, 25);
 		this.setPreferredSize(new Dimension(700, 550));
 		this.setLayout(new BorderLayout());
 		this.setBackground(new Color(40, 40, 40));
-		
+
 		JPanel northPanel = new JPanel(new GridBagLayout());
 		northPanel.setPreferredSize(new Dimension(700, 300));
 		this.add(northPanel, BorderLayout.NORTH);
-		
+
 		JTextArea convoArea = new JTextArea();
-		JScrollPane convoPane = new JScrollPane(convoArea, ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS, 
+		JScrollPane convoPane = new JScrollPane(convoArea, ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS,
 				ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
 		convoPane.setPreferredSize(new Dimension(650, 250));
 		convoArea.setBorder(BorderFactory.createLoweredBevelBorder());
 		convoArea.setFont(textFont);
 		convoArea.setLineWrap(true);
+		convoArea.setEditable(false);
 		convoArea.setWrapStyleWord(true);
 		northPanel.add(convoPane);
-		
+
 		JPanel centerPanel = new JPanel(new GridBagLayout());
 		centerPanel.setPreferredSize(new Dimension(700, 100));
 		this.add(centerPanel, BorderLayout.CENTER);
-		
+
 		JTextArea messageArea = new JTextArea();
-		JScrollPane messagePane = new JScrollPane(messageArea, ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS, 
+		JScrollPane messagePane = new JScrollPane(messageArea, ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS,
 				ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
 		messagePane.setPreferredSize(new Dimension(650, 100));
 		messageArea.setBorder(BorderFactory.createLoweredBevelBorder());
@@ -50,22 +52,24 @@ public class ChatTab extends JPanel implements ActionListener {
 		messageArea.setLineWrap(true);
 		messageArea.setWrapStyleWord(true);
 		centerPanel.add(messagePane);
-		
+
 		JPanel southPanel = new JPanel(null);
 		southPanel.setPreferredSize(new Dimension(700, 75));
-		this.add(southPanel, BorderLayout.SOUTH);		
-		
+		this.add(southPanel, BorderLayout.SOUTH);
+
 		sendButton = new JButton("Send");
 		sendButton.setSize(new Dimension(120, 35));
 		sendButton.setLocation(532, 20);
 		sendButton.addActionListener(this);
-		southPanel.add(sendButton);		
+		southPanel.add(sendButton);
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		if(e.getSource() == sendButton) {
-			
+			request = messageArea.getText(); // messageArea needs to be seen by this actionPerformed
+			out.writeObject(request); //'out' needs to be seen via a connection to ChatMain
+			messageArea.setText("");
 		}
 	}
 }
