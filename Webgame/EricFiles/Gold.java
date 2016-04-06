e96a0bc0ca6dc94b73468b6c0a6b8b445a1ad51f
nsteward96 <treck96@gmail.com>
2016-04-06 18:37:03 -0400

    Gold work, images from Kyle

--- Webgame/EricFiles/Gold.java
+++ Webgame/EricFiles/Gold.java
@@ -1,23 +1,45 @@
 import java.util.Random;
 import java.lang.Math;
+import javax.swing.ImageIcon;
 
 public class Gold
 {
-	boolean alive = false;
-	int xPos = 0;
-	int yPos = 0;
+	private ImageIcon gold = new ImageIcon(getClass().getResource("images/CoinGif.gif")); //Image
+	private boolean alive; //Status of gold - alive or dead
+	private int xPos; // x-position
+	private int yPos; //y-position
 
+	public Gold() //Default Constructor
+	{
 
-
+	}
 
 	public void regenerateGold()	//Respawns gold in random new location
 	{
 		Random r = new Random();
-		double newX = r.nextInt(500);
-		double newY = r.nextInt(500);
+		int newX = r.nextInt(500);
+		int newY = r.nextInt(500);
 		xPos = 10*(newX/10);
 		yPos = 10*(newY/10);
 		alive = true;
+	}
+
+	public boolean getState()
+	{
+		return this.alive;
+	}
+	public void setState(boolean s)
+	{
+		this.alive = s;
+	}
+
+	public int getX()
+	{
+		return this.xPos;
+	}
+	public int getY()
+	{
+		return this.yPos;
 	}
 
 }
